a895cf9a97caab119107a7fd3684d1f9960f9635
Sean M. Montgomery <produceconsumerobot@gmail.com>
2016-05-13 18:07:32 -0400

    broadcasting heartbeats.

--- IXUnitLex1/src/ofApp.cpp
+++ IXUnitLex1/src/ofApp.cpp
@@ -166,6 +166,20 @@ void ofApp::draw() {
             } else {
                 blinkLED->setval_gpio("1");
                 blinkLEDoutState = true;
+                // Load the packet data
+                LivestreamNetwork::PacketNoPayload_V1 outPacket;
+                outPacket.hdr.timeStamp = ofGetElapsedTimeMillis();
+                outPacket.hdr.packetCount = ++nPacketsSent;
+                outPacket.hdr.protocolVersion = packetProtocolVersion;
+                strncpy(outPacket.hdr.typeTag, LivestreamNetwork::PONG, 
+                sizeof LivestreamNetwork::PONG / sizeof LivestreamNetwork::PONG[0]);
+            
+        // Send the packet
+        udpBroadcaster.Send((char*) &outPacket, sizeof(outPacket));
+        // Convert the typeTage char[2] to a string for logging
+        typeTag = string(outPacket.hdr.typeTag, outPacket.hdr.typeTag + sizeof(outPacket.hdr.typeTag) / 
+            sizeof(outPacket.hdr.typeTag[0]));
+        ofLog(OF_LOG_VERBOSE) << typeTag << ">>" << "Broadcast" << endl;
             }
         }            
         blinkTimer = ofGetSystemTimeMicros();
