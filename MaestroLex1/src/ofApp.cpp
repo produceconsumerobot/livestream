a1018a61db0b8545b5b48aa656655d1ffebb234d
Sean M. Montgomery <produceconsumerobot@gmail.com>
2016-05-22 10:05:30 -0400

    spacing fix

--- MaestroLex1/src/ofApp.cpp
+++ MaestroLex1/src/ofApp.cpp
@@ -29,7 +29,7 @@ void ofApp::setup(){
 	packetProtocolVersion = 1;
 
 	// Draw the run data to the screen
-	int panelSpacing = 195;
+	int panelSpacing = 190;
 	panelWidth = panelSpacing - 5;
 	panelRowHeight = 300;
 
