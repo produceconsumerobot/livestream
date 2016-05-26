0341fce393c6ec95ab757ff598ed23ab3350e5c3
Sean M. Montgomery <produceconsumerobot@gmail.com>
2016-05-26 15:37:49 -0400

    fixes.

--- IXUnitLex1/src/ofApp.cpp
+++ IXUnitLex1/src/ofApp.cpp
@@ -368,7 +368,7 @@ void ofApp::draw() {
                         // Set the Volume
                         volSound.setVolume(volume);
 						ofSoundSetVolume(volume);
-                        volSound.loadSound(str(inPacket->filePath));
+                        volSound.loadSound(std::string str(inPacket->filePath));
 						volSound.play();
 						volSound.setVolume(volume);
 						ofSoundSetVolume(volume);
